dec0d63a7a36340fe5dd59d9685ac34405f3370e
unknown <wafaaatta04@gmail.com>
2024-04-29 11:52:14 +0200

    entité products

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function(){
+
+    Route::get('/welcome', function (Request $request) {
+        return 'Stock Management App';
+    });
+});
